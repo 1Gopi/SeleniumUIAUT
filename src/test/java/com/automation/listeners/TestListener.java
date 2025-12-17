a16f3df08c31ed8f59604eb3c61e02a1e814ac2c
1Gopi <gopim6056@gmail.com>
2025-12-17 18:36:37 +0530

    final commit

--- src/test/java/com/automation/listeners/TestListener.java
+++ src/test/java/com/automation/listeners/TestListener.java
@@ -3,8 +3,13 @@ import com.framework.utils.ExtentReportManager;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.framework.core.BaseTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -15,24 +20,44 @@ import org.testng.ITestResult;
 public class TestListener implements ITestListener {
 
     // These must be static so they persist across all tests
-    private static ExtentReports extent = ExtentReportManager.createInstance("extent-report.html");
-    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+  //  private static ExtentReports extent = ExtentReportManager.createInstance("extent-report.html");
+   // private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+    
+    static Map<Integer, ExtentTest> extentTestMap = new HashMap<>();
+    static ExtentReports extent = ExtentReportManager.createInstance("extent-report.html");
 
+    
     @Override
     public void onTestStart(ITestResult result) {
         // Create a new entry in the report for this specific test method
-        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
-        test.set(extentTest);
+       // ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        startTest(result.getMethod().getMethodName());
+       // test.set(extentTest);
+        
+    }
+    
+
+    public static synchronized ExtentTest getTest() {
+        return extentTestMap.get((int) Thread.currentThread().getId());
+    }
+    
+    public static synchronized ExtentTest startTest(String testName) {
+        ExtentTest test = extent.createTest(testName);
+        extentTestMap.put((int) Thread.currentThread().getId(), test);
+        return test;
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        test.get().log(Status.PASS, "Test Passed");
+    	  Object currentClass = result.getInstance();
+    	 WebDriver driver = ((BaseTest) currentClass).getDriver();
+    	String base64Screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
+        getTest().log(Status.PASS, "Test Passed",MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        test.get().fail(result.getThrowable()); // Log the error message
+        getTest().fail(result.getThrowable()); // Log the error message
 
         // SENIOR LEVEL: Retrieve the driver from the test class to take a screenshot
         try {
@@ -44,17 +69,21 @@ public class TestListener implements ITestListener {
             // 3. Take Screenshot
             if (driver != null) {
                 String base64Screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
-                test.get().addScreenCaptureFromBase64String(base64Screenshot, "Failure Screenshot");
+                getTest().addScreenCaptureFromBase64String(base64Screenshot, "Failure Screenshot");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    public static synchronized void flush() {
+        extent.flush();
+    }
 
     @Override
     public void onFinish(ITestContext context) {
         if (extent != null) {
-            extent.flush(); // CRITICAL: Writes the file to disk!
+           // extent.flush(); // CRITICAL: Writes the file to disk!
+            flush();
         }
     }
 }
--- src/test/java/com/automation/tests/LoginTest.java
+++ src/test/java/com/automation/tests/LoginTest.java
@@ -1,10 +1,18 @@
 package com.automation.tests;
 
+import com.automation.listeners.TestListener;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
 import com.framework.core.BaseTest;
 import com.framework.pages.LoginPage;
 import com.framework.utils.TestDataProvider;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider; // 1. Import this
 import org.testng.annotations.Test;
@@ -14,6 +22,15 @@ public class LoginTest extends BaseTest {
 
     // 2. Define the Data Provider
     
+	// Initialize Log4j Logger
+    private static final Logger logger = LogManager.getLogger(LoginTest.class);
+   // private static ExtentTest test = TestListener.getTest();
+    
+    public void getScreenshot(String message) {
+    	TakesScreenshot ts = (TakesScreenshot) driver;
+    	String base64Screenshot = ts.getScreenshotAs(OutputType.BASE64);
+    	TestListener.getTest().log(Status.INFO, message, MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
+    }
 
     // 3. Link Test to Data & Add Arguments
 	@Test(dataProvider = "loginData", dataProviderClass = TestDataProvider.class)
@@ -25,6 +42,9 @@ public class LoginTest extends BaseTest {
 
 	    // 2. Perform Login (Use the business method we created!)
 	    loginPage.login(username, password);
+	    logger.info("Logged to Application");
+	    getScreenshot("Logged to App");
+	    //test.addScreenCaptureFromBase64String(username, password)
 	    
 	    // 3. Verify
 	 // 3. Verify
@@ -38,6 +58,7 @@ public class LoginTest extends BaseTest {
 
 	    softAssert.assertAll();
         // Simple check for demonstration
+	    getScreenshot("Did a assert");
         boolean isSecure = driver.getCurrentUrl().contains("secure");
         
         // Logic: Only "tomsmith" should pass, others might fail/stay on page
